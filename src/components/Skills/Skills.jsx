ab928b0fcc410ba9356a415e43b6d66598e1fc84
Nathan <nathanbenson.affiliate@gmail.com>
2021-06-11 23:28:58 +0200

    addjust SKills component and added image to skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Skills.scss';
 import { motion } from "framer-motion";
 
+import webdeveloperImage from '../../assets/web-developer.svg'
 import { FaHtml5, FaCss3Alt, FaSass, FaReact} from 'react-icons/fa';
 import { SiJavascript, SiRedux } from 'react-icons/si';
 import SkillsIcon from './SkillsIcon/SkillsIcon';
@@ -10,14 +11,15 @@ function Skills() {
    
     return (
         <div className="skills">
-            <h1 className="skills__title"><div></div><span>My</span> Skills</h1>
+            
+            <h1 className="skills__title"><img src={webdeveloperImage}/><div></div><span>My</span> Skills</h1>
             <div className="skills__container">
-                <SkillsIcon icon={<FaHtml5 className="icon" style={{color: '#E75327'}} />} text={'HTML5'}/>
-                <SkillsIcon icon={<FaCss3Alt className="icon" style={{color: '#71B2DE'}} />} text={'CSS3'}/>
-                <SkillsIcon icon={<SiJavascript className="icon" style={{color: '#F7DF1D'}} />} text={'JavaScript'}/>
-                <SkillsIcon icon={<FaSass className="icon" style={{color: '#CC6798'}} />} text={'SCSS'}/>
-                <SkillsIcon icon={<FaReact className="icon" style={{color: '#68E1FE'}} />} text={'React'}/>
-                <SkillsIcon icon={<SiRedux className="icon" style={{color: '#774ABD'}} />} text={'Redux'}/>
+                <SkillsIcon icon={<FaHtml5 className="icon" style={{color: '#E75327'}} />} text={'HTML5'} textColor={"#E75327"}/>
+                <SkillsIcon icon={<FaCss3Alt className="icon" style={{color: '#71B2DE'}} />} text={'CSS3'} textColor={"#71B2DE"}/>
+                <SkillsIcon icon={<SiJavascript className="icon" style={{color: '#F7DF1D'}} />} text={'JavaScript'} textColor={"#F7DF1D"}/>
+                <SkillsIcon icon={<FaSass className="icon" style={{color: '#CC6798'}} />} text={'SCSS'} textColor={"#CC6798"}/>
+                <SkillsIcon icon={<FaReact className="icon" style={{color: '#68E1FE'}} />} text={'React'} textColor={"#68E1FE"}/>
+                <SkillsIcon icon={<SiRedux className="icon" style={{color: '#774ABD'}} />} text={'Redux'} textColor={"#774ABD"}/>
             </div>
             
         </div>
--- src/components/Skills/SkillsIcon/SkillsIcon.jsx
+++ src/components/Skills/SkillsIcon/SkillsIcon.jsx
@@ -5,7 +5,7 @@ import { FaHtml5, FaCss3Alt, FaSass, FaReact} from 'react-icons/fa';
 
 import { motion } from "framer-motion";
 
-function SkillsIcon({ icon, text}) {
+function SkillsIcon({ icon, text, textColor}) {
     return (
         <div className="skill-container">
                     <motion.div
@@ -15,7 +15,7 @@ function SkillsIcon({ icon, text}) {
                         }}
                     >{icon}
                     </motion.div>
-                    <span>{text}</span>
+                    <span style={{color: textColor}}>{text}</span>
                 </div>
     )
 }
